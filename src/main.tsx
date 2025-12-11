0c8a9b049dcbb0e940f5c78f91ff2199d7046ab8
Liu Yihao <liuyh970615@gmail.com>
2025-12-11 14:42:40 +0800

    fix: provider position

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HeroUIProvider} from '@heroui/react';
 
 import './index.css';
 import 'leaflet/dist/leaflet.css'; // Leaflet default styles
 import './i18n';
 import {applyTheme} from "./utils/preTheme";
-import {DataModeProvider} from "./hooks/useDataMode";
-import {ThemeProvider} from "@/context/ThemeContext";
-import {GameMapProvider} from "@/context/GameMapContext.tsx";
-import {UserProvider} from "@/context/UserContext";
-import {SiteConfigProvider} from "@/context/SiteConfigContext.tsx";
 
 import {RouterProvider, createRouter} from '@tanstack/react-router'
 
@@ -36,19 +30,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <SiteConfigProvider>
-        <UserProvider>
-          <DataModeProvider>
-            <GameMapProvider>
-              <ThemeProvider>
-                <HeroUIProvider>
-                  <RouterProvider router={router}/>
-                </HeroUIProvider>
-              </ThemeProvider>
-            </GameMapProvider>
-          </DataModeProvider>
-        </UserProvider>
-      </SiteConfigProvider>
+      <RouterProvider router={router}/>
     </React.StrictMode>,
   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,36 @@
 import {createRootRoute, Outlet, Navigate} from '@tanstack/react-router'
 import TopNavbar from "@/components/TopNavbar.tsx";
+import {SiteConfigProvider} from "@/context/SiteConfigContext.tsx";
+import {UserProvider} from "@/context/UserContext.tsx";
+import {DataModeProvider} from "@/hooks/useDataMode.tsx";
+import {GameMapProvider} from "@/context/GameMapContext.tsx";
+import {ThemeProvider} from "@/context/ThemeContext.tsx";
+import {HeroUIProvider} from "@heroui/react";
 // import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 // A separate component to handle the redirect logic
 const NotFoundRedirect = () => {
-  return <Navigate to="/" replace />;
+  return <Navigate to="/" replace/>;
 };
 
 const RootLayout = () => (
   <>
-    <div className="h-screen w-screen flex flex-col">
-      <TopNavbar/>
-      <Outlet/>
-    </div>
+    <SiteConfigProvider>
+      <UserProvider>
+        <DataModeProvider>
+          <GameMapProvider>
+            <ThemeProvider>
+              <HeroUIProvider>
+                <div className="h-screen w-screen flex flex-col">
+                  <TopNavbar/>
+                  <Outlet/>
+                </div>
+              </HeroUIProvider>
+            </ThemeProvider>
+          </GameMapProvider>
+        </DataModeProvider>
+      </UserProvider>
+    </SiteConfigProvider>
     {/*<TanStackRouterDevtools />*/}
   </>
 )
