008e5ff9204e5a9e4e2bd2dded771293b6bd5545
Liu Yihao <liuyh970615@gmail.com>
2025-12-13 12:35:12 +0800

    fix: github version

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,10 @@ import {RouterProvider, createRouter} from '@tanstack/react-router'
 import {routeTree} from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({routeTree})
+const router = createRouter({
+  basepath: import.meta.env.BASE_URL,
+  routeTree
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
